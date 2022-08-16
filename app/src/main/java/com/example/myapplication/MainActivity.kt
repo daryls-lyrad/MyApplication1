ca2cb3caff2651318183817ca5ed608fbde271cc
anreh <anurehka@gmail.com>
2022-08-16 10:53:17 +0800

    20220816 -DATA BINDING ADDED -TOTAL SCORE WITH DICE ROLL

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,23 +6,31 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
     lateinit var editPlayerName: EditText
     lateinit var playerNameTV: TextView
+    lateinit var binding: ActivityMainBinding
+    private val gameInfo:MyName = MyName("Top G")
+    private var totalScore: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView( this,R.layout.activity_main)
         diceImg = findViewById(R.id.diceImg)
         numberText = findViewById(R.id.numberText)
         editPlayerName = findViewById(R.id.editPlayerName)
         playerNameTV = findViewById(R.id.playerName)
-        val rollButton: Button = findViewById(R.id.rollButton)
-        rollButton.setOnClickListener { rollDice() }
-        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
-        updateButton.setOnClickListener { updatePlayerName(it) }
+        /* val rollButton: Button = findViewById(R.id.rollButton)*/
+        binding.gameInfo = gameInfo
+        binding.rollButton.setOnClickListener { rollDice() }
+        /*rollButton.setOnClickListener { rollDice() }*/
+        /*val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)*/
+        binding.updatePlayerNameBtn.setOnClickListener { updatePlayerName(it) }
     }
 
     private fun rollDice() {
@@ -30,6 +38,9 @@ class MainActivity : AppCompatActivity() {
         //val numberText: TextView = findViewById(R.id.numberText)
         //val diceImg: ImageView = findViewById(R.id.diceImg)
         numberText.text = randomNum.toString()
+        totalScore += randomNum
+        binding.numberText.text = totalScore.toString()
+
 
         val imgSrc = when (randomNum) {
             1 -> R.drawable.dice_1
@@ -39,11 +50,17 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImg.setImageResource(imgSrc)
+        binding.diceImg.setImageResource(imgSrc)
     }
 
     private fun updatePlayerName(view: View) {
-        playerNameTV.text = editPlayerName.text
+//        binding.playerName.text = binding.editPlayerName.text
+//        playerNameTV.text = editPlayerName.text
+        binding.apply {
+            gameInfo?.playerName = binding.editPlayerName.text.toString()
+            invalidateAll()
+        }
+
 
         editPlayerName.setText(" ")
         editPlayerName.clearFocus()
---
+++ app/src/main/java/com/example/myapplication/MyName.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+class MyName(var playerName: String="", var totalScore: String="") {
+}
